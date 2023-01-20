dfd916f3b266ca79d11c22b8569205e88ca3d69a
tanongsak intean <tanongsakintean.5333@gmail.com>
2023-01-21 00:27:05 +0700

    set responsive

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -38,7 +38,7 @@ export default function Home() {
             hasMore={videos.length < 500}
             loader={<Spinner />}
           >
-            <div className="grid gap-y-14 gap-x-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 p-8">
+            <div className="grid gap-y-14 gap-x-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5  mr-0     p-8">
               {videos.map((item: HomePageVideos) => {
                 return <Card data={item} key={item.videoId} />;
               })}
