07f8c5be0495280f82ec48a1944d3fea03eed06f
Tanongsak Intean <71477852+tanongsakintean@users.noreply.github.com>
2023-01-21 14:21:02 +0700

    Update getHomePageVideos.ts

--- src/store/reducers/getHomePageVideos.ts
+++ src/store/reducers/getHomePageVideos.ts
@@ -16,7 +16,7 @@ export const getHomePageVideos = createAsyncThunk(
     const {
       data: { items, nextPageToken },
     } = await axios.get(
-      `${YOUTUBE_API_URL}/search?maxResults=20&q="batman"&key=${API_KEY}&part=snippet&type=video&${
+      `${YOUTUBE_API_URL}/search?maxResults=20&q=""&key=${API_KEY}&part=snippet&type=video&${
         isNext ? `pageToken=${nextPageTokenFromState}` : ""
       }`
     );
